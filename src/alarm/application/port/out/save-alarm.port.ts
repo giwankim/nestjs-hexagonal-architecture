9f32cf33dec048925375b45f6df686f702a536d7
Giwan Kim <giwan.kim@gmail.com>
2024-01-20 10:34:27 +0900

    fix(alarm): interface method signature that was missing alarm domain model that should be persisted

--- src/alarm/application/port/out/save-alarm.port.ts
+++ src/alarm/application/port/out/save-alarm.port.ts
@@ -3,5 +3,5 @@ import { Alarm } from '../../domain/model/alarm';
 export const SAVE_ALARM_PORT = Symbol('SAVE_ALARM_PORT');
 
 export interface SaveAlarmPort {
-  saveAlarm(): Promise<Alarm>;
+  saveAlarm(alarm: Alarm): Promise<Alarm>;
 }
